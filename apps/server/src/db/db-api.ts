1fc72a24305405f5276c305d0c6e08ff16a8b97c
Craig Ostrin <craigostrin@gmail.com>
2023-11-28 17:50:06 +0100

    setup(db): add firebase and '/users' endpoint
    /users => all users

---
+++ apps/server/src/db/db-api.ts
@@ -0,0 +1,10 @@
+import { collection, getDocs } from 'firebase/firestore';
+import db from './firebase';
+
+export async function getAllUsers() {
+  const usersRef = collection(db, "users")
+  const querySnapshot = await getDocs(usersRef)
+
+  const users = querySnapshot.docs.map(doc => doc.data())
+  return users
+}
