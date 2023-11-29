4f239e27b8ff60328479b2c5a515ddb71c101bd7
Clem Fandango <drosshere@gmail.com>
2023-11-29 16:54:02 +0100

    Install Zod, Add Form Schema

--- apps/client/src/pages/CreateProfile.tsx
+++ apps/client/src/pages/CreateProfile.tsx
@@ -1,10 +1,24 @@
 import Header1 from "@/components/Header1"
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { log } from "console"
+
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
 
 export default function CreateProfile() {
-  return (
-    <>
-      <h1>Hi</h1>
-      <p className="p-32"></p>
-    </>
-  )
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
+  return <></>
 }
