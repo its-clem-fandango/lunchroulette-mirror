2e8012913242c0b09c92b1a4f953ba427048d943
Clem Fandango <drosshere@gmail.com>
2023-11-29 11:08:53 +0100

    eslint-prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,11 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   ignorePatterns: ["apps/**", "packages/**"],
-  extends: ["@repo/eslint-config/library.js"],
+  extends: ["@repo/eslint-config/library.js", "prettier"],
+  rules: {
+    "no-extra-semi": "error",
+    "no-mixed-spaces-and-tabs": "error",
+  },
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
