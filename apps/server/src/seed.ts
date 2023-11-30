dad2b1221235420a7efc34f6d7964321e9da6a5c
Victor Bettiol <victor.bettiol@hotmail.com>
2023-11-30 14:57:10 +0000

    Provide edit profile option initial commit

---
+++ apps/server/src/seed.ts
@@ -0,0 +1,12 @@
+import { faker } from '@faker-js/faker'
+
+export async function seed () {
+  const userKey = 'users'
+
+  const usersTemplate = new Array(21).fill('')
+
+  const users = usersTemplate.map(() => ({
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName()
+  }))
+}
