e399f69e421abcfe8a3208be97f2b6e9350c8287
Aniol Quer <aniolquer@gmail.com>
2023-12-01 11:20:59 +0100

    added POST request to the sign up for lunch today button, reorganized distribution of buttons, added titles

--- apps/client/src/pages/Timepreferences.tsx
+++ apps/client/src/pages/Timepreferences.tsx
@@ -1,25 +1,44 @@
-import { Link } from "react-router-dom"
-import { Button } from "@/components/ui/button"
+import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
 
 export default function TimePreferences() {
-    return (
-        <>
-            <h1 className="text-3xl text-amber-900 text-center">Time Preferences </h1>
-            <form className="text-center" action="" method="post">
-                <select name="" id="">
-                    <option value="Wednesday">Wednesday 1pm to 2pm</option>
-                    <option value="Wednesday">Wednesday 2pm to 3pm</option>
-                    <option value="Thursday">Thursday 1pm to 2pm</option>
-                    <option value="Thursday">Thursday 2pm to 3pm</option>
-                </select>
 
-                <div className="text-center">
-                    <Link to="/lunch" >  {/* set API for receiing this */}
+
+
+    return (
+
+
+        // SELECT DROP DOWN
+        <div className="flex items-center justify-center">
+            <div>
+                <h1 className="text-3xl text-amber-900 mb-5">Time Preferences</h1>
+                <div className="text-center mb-5">
+                    <Select>
+                        <SelectTrigger className="w-[180px]">
+                            <SelectValue placeholder="Pick your preference" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            <SelectItem value="Thursday 2pm to 3pm">Thursday 2pm to 3pm</SelectItem>
+                            <SelectItem value="Thursday 2pm to 3pm">Thursday 2pm to 3pm</SelectItem>
+                            <SelectItem value="Thursday 2pm to 3pm">Thursday 2pm to 3pm</SelectItem>
+                            <SelectItem value="Thursday 2pm to 3pm">Thursday 2pm to 3pm</SelectItem>
+                        </SelectContent>
+                    </Select>
+                </div>
+                <div >
+                    <Link to="/lunch">
                         <Button className="p-5 m-3">Save Preferences</Button>
                     </Link>
-                 </div>
-            </form>
-           
-        </>
-    )}
-
+                </div>
+            
+            </div>
+        </div>
+    );
+}
