d26edb5ab97595db898d9f36a751fd3551128056
Clem Fandango <drosshere@gmail.com>
2023-12-05 12:47:22 +0100

    Add matchId and lastMatched

--- apps/client/src/pages/Homepage.tsx
+++ apps/client/src/pages/Homepage.tsx
@@ -1,50 +1,54 @@
-import ConfirmLunchButton from "@/components/ConfirmLunchButton";
-import { Button } from "../components/ui/button";
+import ConfirmLunchButton from "@/components/ConfirmLunchButton"
+import { Button } from "../components/ui/button"
 import { Link } from "react-router-dom"
 
 export default function Homepage() {
-    return (
-        <>
-                <h1 className="text-2xl text-teal-800 text-center">Homepage</h1>
+  //for logged in user: if isAvailableForToday is falsy, then I can see a button inviting me to join
+  //so I have to fetch my ID
 
-                    {/* EDIT PROFILE  */}
-                    <div className="text-center">
-                        <Link to="/profile">
-                            <Button className="p-5 text-center m-3">⚙️ Edit Profile</Button>
-                        </Link>
-                    </div>
+  const loggedInUserID = "08f1f380-88b4-4a1b-8d8a-77e4b2751698"
 
+  return (
+    <>
+      <h1 className="text-2xl text-teal-800 text-center">Homepage</h1>
 
-                    {/* REGISTER FOR LUNCH MEETING  */}
-                    <hr />
-                    <div className="text-center">
-                        <h1 className="text-2xl text-teal-800 m-5">Join us for Lunch Today</h1>
-                        {/* <Link to="/lunch">
+      {/* EDIT PROFILE  */}
+      <div className="text-center">
+        <Link to="/profile">
+          <Button className="p-5 text-center m-3">⚙️ Edit Profile</Button>
+        </Link>
+      </div>
+
+      {/* REGISTER FOR LUNCH MEETING  */}
+      <hr />
+      <div className="text-center">
+        <h1 className="text-2xl text-teal-800 m-5">Join us for Lunch Today</h1>
+        {/* <Link to="/lunch">
                             <Button className="p-5 m-3">🥙 Go have lunch today</Button>
                         </Link> */}
-                        <ConfirmLunchButton /> 
-                    </div>
+        <ConfirmLunchButton />
+      </div>
 
+      {/* RECURRING PREFERENCES  */}
+      <div className="text-center">
+        <h1 className="text-2xl text-teal-800 m-5">
+          Or Establish Recurring Preferences
+        </h1>
+        <Link to="/preferences">
+          <Button className="text-center m-3">
+            🕰️ Define Time Preferences
+          </Button>
+        </Link>
+      </div>
 
-                    {/* RECURRING PREFERENCES  */}
-                    <div className="text-center">
-                        <h1 className="text-2xl text-teal-800 m-5">Or Establish Recurring Preferences</h1>
-                        <Link to="/preferences">
-                            <Button className="text-center m-3">🕰️ Define Time Preferences</Button>
-                        </Link>
-                    </div>
-                    
-
-                    {/* NEXT MEETING INFO  */}
-                    <hr />
-                    <div className="text-center">
-                        <h1 className="text-2xl text-teal-800 m-5">Here's your next Meeting</h1>
-                        <Link to="/meeting">
-                            <Button className="p-5 m-3">📅 View your meeting info</Button>
-                        </Link>
-                    </div>
-
-        </>
-    )
-  }
-
+      {/* NEXT MEETING INFO  */}
+      <hr />
+      <div className="text-center">
+        <h1 className="text-2xl text-teal-800 m-5">Here's your next Meeting</h1>
+        <Link to="/meeting">
+          <Button className="p-5 m-3">📅 View your meeting info</Button>
+        </Link>
+      </div>
+    </>
+  )
+}
--- apps/server/src/db/seed.ts
+++ apps/server/src/db/seed.ts
@@ -1,18 +1,20 @@
-import { faker } from '@faker-js/faker'
-import UsersController from '../controllers/users.controller'
+import { faker } from "@faker-js/faker"
+import UsersController from "../controllers/users.controller"
 
-console.log('Seeding: ')
+console.log("Seeding: ")
 seed(18)
 
-export async function seed (n: number) {
-  const userKey = 'users'
+export async function seed(n: number) {
+  const userKey = "users"
 
-  const usersTemplate = new Array(n).fill('')
+  const usersTemplate = new Array(n).fill("")
 
   const users = usersTemplate.map(() => ({
     firstName: faker.person.firstName(),
     lastName: faker.person.lastName(),
     isAvailableToday: false,
+    matchId: "c1d2e3f4-g5h6-i7j8-k9l0-m1n2o3p4q5r6",
+    lastMatched: "2019-01-01T00:00:00.000Z",
   }))
 
   UsersController.createMultipleUsers(users)
