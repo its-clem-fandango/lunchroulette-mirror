87a4fce5eab2ae7d1847060e0da3dee3f772993a
Clem Fandango <drosshere@gmail.com>
2023-12-07 17:07:15 +0100

    Add No Match Message to View Meeting

--- apps/client/src/pages/ViewMeeting.tsx
+++ apps/client/src/pages/ViewMeeting.tsx
@@ -2,52 +2,58 @@ import { apiUrl } from "@/lib/constants"
 import TodaysLunch from "@/components/TodaysLunch"
 import { Match } from "@/lib/types"
 import { useEffect, useState } from "react"
-
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { log } from "console"
+import { Card, CardHeader, CardTitle } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
 
 const TIME = "12:00 PM"
 const LOCATION = "Honest Greens"
-const loggedInUserID = "08f1f380-88b4-4a1b-8d8a-77e4b2751698"
+const loggedInUserID = "2633686d-492c-4eec-a79c-0ba5ce4b30fd"
 
 export default function ViewMeeting() {
-  /*  const lunchPartner: RequestInit = {
-    method: "GET",
-  }
-
-  //const response = await fetch(`${apiUrl}/`, lunchPartner) // Changed 'getrequest' to 'lunchPartner'
-
-  if (response.ok) {
-    const responseData = await response.json()
-    const partnersName = responseData.name
-  } else {
-    console.error("Request failed with a status:", response.status) // Removed the extra '(404)'
-  } */
+  /* I guess the controller should check whether or not 
+  the forever-alone state is seen. It asks the model for
+  the match ID and if their isn't one then 
+  it shows them they have no notifications today */
 
   const [matches, setMatches] = useState<Match[]>([])
+  const [noMatch, setNoMatch] = useState(false)
 
   useEffect(() => {
     async function getMatch() {
       const options = { headers: { "Content-Type": "application/json" } }
-      const res = await fetch(`${apiUrl}/getMatch/${loggedInUserID}`, options)
-      const data = await res.json()
-      console.log(data)
-      console.log(data.match.firstName)
 
-      setMatches([
-        {
-          name: `${data.match.firstName} ${data.match.lastName}`,
-          picUrl: "https://via.placeholder.com/40",
-        },
-      ])
-      //use data to set some state, which conditionally renders TodaysLunch
+      try {
+        const res = await fetch(`${apiUrl}/getMatch/${loggedInUserID}`, options)
+
+        if (!res.ok) {
+          // Handle 404 error
+          if (res.status === 404) {
+            setNoMatch(true)
+            console.log("no match is true")
+            return
+          }
+          throw new Error(`HTTP error! Status: ${res.status}`)
+        }
+
+        const data = await res.json()
+        console.log(data)
+
+        if (!data.match) {
+          setNoMatch(true)
+          console.log("no match is true")
+        } else {
+          setMatches([
+            {
+              name: `${data.match.firstName} ${data.match.lastName}`,
+              picUrl: "https://via.placeholder.com/40",
+            },
+          ])
+          // use data to set some state, which conditionally renders TodaysLunch
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error)
+        // Handle the error in a way that makes sense for your application
+      }
     }
 
     getMatch()
@@ -55,11 +61,19 @@ export default function ViewMeeting() {
 
   return (
     <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <TodaysLunch matches={matches} time={TIME} location={LOCATION} />
+      {noMatch ? (
+        <Card className="w-[350px] shadow-green-600">
+          <CardHeader>
+            <CardTitle>Sorry, we couldn't find a match for you today</CardTitle>
+          </CardHeader>
+        </Card>
+      ) : (
+        <TodaysLunch matches={matches} time={TIME} location={LOCATION} />
+      )}
     </div>
   )
 }
 
 // function NoLunchToday({}) {
 //   return <div>Sorry, you have no matches today!</div>
-// }
+//
