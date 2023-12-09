965fbcbe8dc7f83c4e4f78fdb62930b3f882088b
Victor Bettiol <victor.bettiol@hotmail.com>
2023-12-09 15:08:56 +0000

    Minor fix, missing file AdminPanel.tsx

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -6,7 +6,8 @@ import LunchConfirmation from "./pages/LunchTodayConfirmation"
 import ViewMeeting from "./pages/ViewMeeting"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
-import AdminPanel from "/Users/juneau/Documents/arol.dev/repos/Projects/lunch-roulette/apps/client/src/tools/AdminPanel.tsx"
+// Missing AdminPanel.tsx
+// import AdminPanel from "/Users/juneau/Documents/arol.dev/repos/Projects/lunch-roulette/apps/client/src/tools/AdminPanel.tsx"
 
 function App() {
   return (
@@ -40,7 +41,7 @@ function App() {
         <Route path="/viewmeeting" element={<ViewMeeting />}></Route>
         <Route path="/sign-in" element={<SignIn />}></Route>
         <Route path="/sign-up" element={<SignUp />}></Route>
-        <Route path="/devcontrolpanel" element={<AdminPanel />}></Route>
+        {/* <Route path="/devcontrolpanel" element={<AdminPanel />}></Route> */}
       </Routes>
     </Router>
   )
