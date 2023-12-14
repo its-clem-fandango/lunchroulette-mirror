425edfbfcfaf1f8c9a118c16e8d278592ad36963
Victor Bettiol <victor.bettiol@hotmail.com>
2023-12-14 06:55:03 +0000

    Add timer for next lunch and timer for matching users

--- apps/client/src/pages/Homepage.tsx
+++ apps/client/src/pages/Homepage.tsx
@@ -9,21 +9,40 @@ import { useNavigate } from "react-router-dom"
 export default function Homepage() {
   const [loading, setLoading] = useState(false)
 
+  const [countdownDate, setCountdownDate] = useState(
+    new Date().setHours(11, 0, 0, 0)
+  )
+  const [timeNow, setTimeNow] = useState(new Date().getTime())
+  const [distance, setDistance] = useState(countdownDate - timeNow)
+  const [hours, setHours] = useState(0)
+  const [minutes, setMinutes] = useState(0)
+  const [seconds, setSeconds] = useState(0)
+
+  setInterval(() => {
+    setTimeNow(new Date().getTime())
+    if (countdownDate - timeNow < 0) {
+      setCountdownDate(new Date().setHours(11, 0, 0, 0) + 86400000)
+    }
+    setDistance(countdownDate - new Date().getTime())
+    setHours(Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)))
+    setMinutes(Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)))
+    setSeconds(Math.floor((distance % (1000 * 60)) / 1000))
+  }, 1000)
+
   const navigate = useNavigate()
 
   function handleSpin() {
     setLoading(true)
+    const randomNumber = Math.floor(Math.random() * 5 * 6000)
+    console.log(randomNumber)
     //TODO: fetch post join the pool
     setTimeout(async () => {
       //do fetch here
+      setLoading(false)
       navigate("/viewmeeting")
-    }, 1000)
+    }, randomNumber)
   }
 
-  function handleTime() {
-    //make dynamic
-    return "2:45"
-  }
   return (
     <>
       <div className="flex flex-col items-center max-w-[480px] w-full mx-auto pt-12">
@@ -34,7 +53,7 @@ export default function Homepage() {
         />
 
         <header className="text-myColor font-semibold text-center text-xl mt-20 ">
-          You're not in the pool.
+          {loading ? `You're in the pool!` : `You're not in the pool`}
         </header>
 
         <main className="flex flex-col mt-20 px-8">
@@ -42,8 +61,12 @@ export default function Homepage() {
             Spin the Wheel to enter the pool
           </header>
           <div className="text-myColor text-center">
-            <span> Next lunch in </span>
-            <span className="font-semibold">{handleTime()}</span>
+            <span>Next lunch in </span>
+            <span className="font-semibold">
+              {hours.toString().padStart(2, "0")}:
+              {minutes.toString().padStart(2, "0")}:
+              {seconds.toString().padStart(2, "0")}
+            </span>
           </div>
         </main>
 
