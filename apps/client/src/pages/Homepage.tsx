44f598f32267583d01faef79ceb6dd59bf27f2a4
Arol Viñolas <arol@arol.dev>
2023-12-16 12:36:06 +0100

    feat(client): header component

--- apps/client/src/pages/Homepage.tsx
+++ apps/client/src/pages/Homepage.tsx
@@ -7,6 +7,8 @@ import { useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { useUserContext } from "@/lib/UserContext"
 import { apiUrl } from "@/lib/constants"
+import { Frame } from "@/components/ui/frame"
+import { Header } from "@/components/Header"
 
 export default function Homepage() {
   const [loading, setLoading] = useState(false)
@@ -46,43 +48,40 @@ export default function Homepage() {
 
   return (
     <>
-      <div className="flex flex-col items-center max-w-[480px] w-full mx-auto pt-12">
-        <img
-          src={rouletteimage}
-          className="w-[81px] mt-2.5"
-          alt="Lunch Roulette Logo"
-        />
-
-        <header className="text-myColor font-semibold text-center text-xl mt-20 ">
-          {loading ? `You're in the pool!` : `You're not in the pool`}
-        </header>
-
-        <main className="flex flex-col mt-20 px-8">
-          <header className="text-myColor font-semibold text-3xl text-center tracking-tight">
-            Spin the Wheel to enter the pool
+      <Frame>
+        <Header />
+        <div className="flex flex-col items-center max-w-[480px] w-full mx-auto pt-12">
+          <header className="text-myColor font-semibold text-center text-xl mt-20 ">
+            {loading ? `You're in the pool!` : `You're not in the pool`}
           </header>
-          <div className="text-myColor text-center">
-            <span>Next lunch in </span>
-            <span className="font-semibold">
-              <CountdownTimer countdownTimestampMs={lunchTime} />
-            </span>
-          </div>
-        </main>
 
-        <Button
-          type="submit"
-          className=" bg-myColor text-white mt-7 px-10 h-10"
-          onClick={handleSpin}
-        >
-          {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-          {loading ? "Spinning!" : "Spin!"}
-        </Button>
-        <img
-          src={roulettewheel}
-          alt="Lunch roulette wheel"
-          className={`mt-20 ${loading && "animate-spin"}`}
-        />
-      </div>
+          <main className="flex flex-col mt-20 px-8">
+            <header className="text-myColor font-semibold text-3xl text-center tracking-tight">
+              Spin the Wheel to enter the pool
+            </header>
+            <div className="text-myColor text-center">
+              <span>Next lunch in </span>
+              <span className="font-semibold">
+                <CountdownTimer countdownTimestampMs={lunchTime} />
+              </span>
+            </div>
+          </main>
+
+          <Button
+            type="submit"
+            className=" bg-myColor text-white mt-7 px-10 h-10"
+            onClick={handleSpin}
+          >
+            {loading && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
+            {loading ? "Spinning!" : "Spin!"}
+          </Button>
+        </div>
+      </Frame>
+      <img
+        src={roulettewheel}
+        alt="Lunch roulette wheel"
+        className={`mt-20 ${loading && "animate-spin"}`}
+      />
     </>
   )
 }
