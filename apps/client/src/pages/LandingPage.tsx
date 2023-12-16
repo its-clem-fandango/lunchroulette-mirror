dc70ff7ce7e7c8055a13af1eef98eda70fb75831
Clem Fandango <drosshere@gmail.com>
2023-12-16 12:13:49 +0100

    Apply Margins to Landing Page + .svg tab icon

--- apps/client/src/pages/LandingPage.tsx
+++ apps/client/src/pages/LandingPage.tsx
@@ -17,17 +17,19 @@ export default function LandingPage() {
   }
 
   return (
-    <div className="items-stretch flex flex-col py-12">
-      <img
-        src={rouletteimage}
-        className="aspect-[2.08] object-contain object-center w-[249px] overflow-hidden self-center max-w-full mt-36"
-      />
-      <header className="text-black text-xl font-semibold leading-7 tracking-normal self-center whitespace-wrap mt-20">
-        Welcome! We built Lunch Roulette so Norrsken members can get to know
-        each other.
-      </header>
-      <div className="items-center flex w-full flex-col justify-center mt-16 mb-40 px-16">
-        <SignIn onSignIn={handleSignIn} />
+    <div className="fixed h-full w-full bg-white py-14 px-10 box-border">
+      <div className="items-stretch flex flex-col py-12">
+        <img
+          src={rouletteimage}
+          className="aspect-[2.08] object-contain object-center w-[249px] overflow-hidden self-center max-w-full mt-36"
+        />
+        <header className="text-black text-xl font-semibold leading-7 tracking-normal self-center whitespace-wrap mt-20">
+          Welcome! We built Lunch Roulette so Norrsken members can get to know
+          each other.
+        </header>
+        <div className="items-center flex w-full flex-col justify-center mt-16 mb-40 px-16">
+          <SignIn onSignIn={handleSignIn} />
+        </div>
       </div>
     </div>
   )
